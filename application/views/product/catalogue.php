728dae6a5f316382bcf9ad88a5559a1f88d1c4ff
mdarmancse <65160577+mdarmancse@users.noreply.github.com>
2021-07-31 21:31:50 +0600

    catalogue serach added

--- application/views/product/catalogue.php
+++ application/views/product/catalogue.php
@@ -43,43 +43,43 @@ Manage Category Start -->
             $this->session->unset_userdata('error_message');
         }
         ?>
-          <div class="row">
+        <div class="row">
             <div class="col-sm-12">
                 <div class="panel panel-default">
-                    <div class="panel-body"> 
+                    <div class="panel-body">
                         <?php echo form_open('CProduct/filter_category_wise', array('class' => 'form-inline', 'method' => 'post')) ?>
                         <?php
                         date_default_timezone_set("Asia/Dhaka");
                         $today = date('Y-m-d');
                         ?>
-                         <div class="col-sm-6">
-                                <div class="form-group row">
-                            <label class="" for="category"><?php echo display('category') ?></label>
-                            <select  name="category" class="form-control" id="category">
-                                <option value="">--select one -- </option>
-                                <?php
-                                foreach ($category_list as $category) {
-                                    ?>
-                                    <option value="<?php echo $category['category_id']; ?>"><?php echo $category['category_name']; ?></option>
-                                <?php } ?>
-                            </select>
-                        </div> 
-                      </div>
-                            <div class="col-sm-4" id="subCat_div">
-                                <div class="form-group row">
-                                    <label for="sub_category_id" class="">Sub Category</label>
-                                    <div class="col-sm-8">
-                                        <select  name="subcategory" class="form-control" id="subcategory">
-                                <option value="">--select one -- </option>
-                                <?php
-                                foreach ($subcategory_list as $subcategory) {
-                                    ?>
-                                    <option value="<?php echo $subcategory['sub_cat_id']; ?>"><?php echo $subcategory['subcat_name']; ?></option>
-                                <?php } ?>
-                            </select>
-                                    </div>
+                        <div class="col-sm-6">
+                            <div class="form-group row">
+                                <label class="" for="category"><?php echo display('category') ?></label>
+                                <select  name="category" class="form-control" id="category">
+                                    <option value="">--select one -- </option>
+                                    <?php
+                                    foreach ($category_list as $category) {
+                                        ?>
+                                        <option value="<?php echo $category['category_id']; ?>"><?php echo $category['category_name']; ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-sm-4" id="subCat_div">
+                            <div class="form-group row">
+                                <label for="sub_category_id" class="">Sub Category</label>
+                                <div class="col-sm-8">
+                                    <select  name="subcategory" class="form-control" id="subcategory">
+                                        <option value="">--select one -- </option>
+                                        <?php
+                                        foreach ($subcategory_list as $subcategory) {
+                                            ?>
+                                            <option value="<?php echo $subcategory['sub_cat_id']; ?>"><?php echo $subcategory['subcat_name']; ?></option>
+                                        <?php } ?>
+                                    </select>
                                 </div>
                             </div>
+                        </div>
                         <button type="submit" class="btn btn-success"><?php echo display('search') ?></button>
                         <a  class="btn btn-warning" href="#" onclick="printDiv('purchase_div')"><?php echo display('print') ?></a>
                         <?php echo form_close() ?>
@@ -89,88 +89,84 @@ Manage Category Start -->
         </div>
 
         <?php foreach ($all_product as $product) { ?>
+            <div class="row panel panel-bd lobidrag catalogue-panel" id="myUL">
+                <div class="col-sm-12 col-md-4">
 
-   <div class="row panel panel-bd lobidrag catalogue-panel" id="myUL">
+                    <div class="image_box" >
 
-   <div class="row panel panel-bd lobidrag catalogue-panel">
+                        <?php echo "<img class='zoom' src='".html_escape($product->image)."' id='img1'  >";?>
+                    </div>
 
-    <div class="col-sm-12 col-md-4">
-
-        <div class="image_box" >
-
-         <?php echo "<img class='zoom' src='".html_escape($product->image)."' id='img1'  >";?>
-        </div>
-
-    </div>
+                </div>
 
 
-    <div class="col-sm-12 col-md-8">
+                <div class="col-sm-12 col-md-8">
 
-        <div class="col-sm-6 col-md-6 " >
+                    <div class="col-sm-6 col-md-6 " >
 
-          <table class="table" width="100%" id="myTable">
-
-
-<!--            <caption  class="resumehead">--><?php //echo display('positional_information')?><!--</caption>-->
-
-                    <tr>
-                        <th>Category</th>
-<!--                        <td>--><?php //echo html_escape($row[0]['designation']);?><!--</td>-->
-                        <td><?php echo html_escape($product->category_name); ?></td>
-                    </tr>
-
-            <?php if($product->subcat_name):?>
-              <tr>
-                  <th>Sub-category</th>
-                  <td><?php echo html_escape($product->subcat_name); ?></td>
-              </tr>
-            <?php endif;?>
-                    <tr>
-                        <th>Product Name</th>
-                        <td><?php echo html_escape($product->product_name); ?></td>
-                    </tr>
+                        <table class="table" width="100%" id="myTable">
 
 
-                    <tr>
-                        <th>Parts No.</th>
-                        <td><?php echo html_escape($product->parts);?></td>
-                    </tr>
-              <tr>
-                  <th>SKU</th>
-                  <td><?php echo html_escape($product->sku);?></td>
-              </tr>
-              <tr>
-                  <th>Unit Type</th>
-                  <td><?php echo html_escape($product->unit); ?></td>
-              </tr>
-                    <tr>
-                        <th>Brand</th>
-                        <td><?php echo html_escape($product->brand_name); ?></td>
-                    </tr>
-                  <tr>
-                      <th>Model</th>
-                      <td><?php echo html_escape($product->model_name); ?></td>
-                  </tr>
-              <tr>
-                  <th>Associated Tag</th>
-                  <td><?php echo html_escape($product->tag);?></td>
-              </tr>
+                            <!--            <caption  class="resumehead">--><?php //echo display('positional_information')?><!--</caption>-->
+
+                            <tr>
+                                <th>Category</th>
+                                <!--                        <td>--><?php //echo html_escape($row[0]['designation']);?><!--</td>-->
+                                <td><?php echo html_escape($product->category_name); ?></td>
+                            </tr>
+
+                            <?php if($product->subcat_name):?>
+                                <tr>
+                                    <th>Sub-category</th>
+                                    <td><?php echo html_escape($product->subcat_name); ?></td>
+                                </tr>
+                            <?php endif;?>
+                            <tr>
+                                <th>Product Name</th>
+                                <td><?php echo html_escape($product->product_name); ?></td>
+                            </tr>
 
 
-</table>
+                            <tr>
+                                <th>Parts No.</th>
+                                <td><?php echo html_escape($product->parts);?></td>
+                            </tr>
+                            <tr>
+                                <th>SKU</th>
+                                <td><?php echo html_escape($product->sku);?></td>
+                            </tr>
+                            <tr>
+                                <th>Unit Type</th>
+                                <td><?php echo html_escape($product->unit); ?></td>
+                            </tr>
+                            <tr>
+                                <th>Brand</th>
+                                <td><?php echo html_escape($product->brand_name); ?></td>
+                            </tr>
+                            <tr>
+                                <th>Model</th>
+                                <td><?php echo html_escape($product->model_name); ?></td>
+                            </tr>
+                            <tr>
+                                <th>Associated Tag</th>
+                                <td><?php echo html_escape($product->tag);?></td>
+                            </tr>
+
+
+                        </table>
 
 
 
 
 
 
-</div>
+                    </div>
 
 
-</div>
+                </div>
 
 
-   </div>
+            </div>
 
         <?php } ?>
 
--- application/views/product/catalogue_search.php
+++ application/views/product/catalogue_search.php
@@ -40,43 +40,43 @@
             $this->session->unset_userdata('error_message');
         }
         ?>
-          <div class="row">
+        <div class="row">
             <div class="col-sm-12">
                 <div class="panel panel-default">
-                    <div class="panel-body"> 
+                    <div class="panel-body">
                         <?php echo form_open('CProduct/filter_category_wise', array('class' => 'form-inline', 'method' => 'post')) ?>
                         <?php
                         date_default_timezone_set("Asia/Dhaka");
                         $today = date('Y-m-d');
                         ?>
                         <div class="col-sm-6">
-                                <div class="form-group row">
-                            <label class="" for="category"><?php echo display('category') ?></label>
-                            <select  name="category" class="form-control" id="category">
-                                <option value="">--select one -- </option>
-                                <?php
-                                foreach ($category_list as $category) {
-                                    ?>
-                                    <option value="<?php echo $category['category_id']; ?>"><?php echo $category['category_name']; ?></option>
-                                <?php } ?>
-                            </select>
-                        </div> 
-                      </div>
-                            <div class="col-sm-4" id="subCat_div">
-                                <div class="form-group row">
-                                    <label for="sub_category_id" class="">Sub Category</label>
-                                    <div class="col-sm-8">
-                                        <select  name="subcategory" class="form-control" id="subcategory">
-                                <option value="">--select one -- </option>
-                                <?php
-                                foreach ($subcategory_list as $subcategory) {
-                                    ?>
-                                    <option value="<?php echo $subcategory['sub_cat_id']; ?>"><?php echo $subcategory['subcat_name']; ?></option>
-                                <?php } ?>
-                            </select>
-                                    </div>
+                            <div class="form-group row">
+                                <label class="" for="category"><?php echo display('category') ?></label>
+                                <select  name="category" class="form-control" id="category">
+                                    <option value="">--select one -- </option>
+                                    <?php
+                                    foreach ($category_list as $category) {
+                                        ?>
+                                        <option value="<?php echo $category['category_id']; ?>"><?php echo $category['category_name']; ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-sm-4" id="subCat_div">
+                            <div class="form-group row">
+                                <label for="sub_category_id" class="">Sub Category</label>
+                                <div class="col-sm-8">
+                                    <select  name="subcategory" class="form-control" id="subcategory">
+                                        <option value="">--select one -- </option>
+                                        <?php
+                                        foreach ($subcategory_list as $subcategory) {
+                                            ?>
+                                            <option value="<?php echo $subcategory['sub_cat_id']; ?>"><?php echo $subcategory['subcat_name']; ?></option>
+                                        <?php } ?>
+                                    </select>
                                 </div>
                             </div>
+                        </div>
                         <button type="submit" class="btn btn-success"><?php echo display('search') ?></button>
                         <a  class="btn btn-warning" href="#" onclick="printDiv('purchase_div')"><?php echo display('print') ?></a>
                         <?php echo form_close() ?>
@@ -84,65 +84,65 @@
                 </div>
             </div>
         </div>
-            <?php foreach ($category_wise as $product) {?>
-   <div class="row panel panel-bd lobidrag catalogue-panel" id="myUL">
+        <?php foreach ($category_wise as $product) {?>
+            <div class="row panel panel-bd lobidrag catalogue-panel" id="myUL">
 
-    <div class="col-sm-12 col-md-4">
+                <div class="col-sm-12 col-md-4">
 
-        <div class="image_box" >
+                    <div class="image_box" >
 
-         <?php echo "<img class='zoom' src='".html_escape($product->image)."' id='img1'  >";?>
-        </div>
+                        <?php echo "<img class='zoom' src='".html_escape($product->image)."' id='img1'  >";?>
+                    </div>
 
-    </div>
-    <div class="col-sm-12 col-md-8">
-        <div class="col-sm-6 col-md-6 " >
-          <table class="table" width="100%" id="myTable">
-                <tr>
-                    <th>Category</th>
-                    <td><?php echo html_escape($product->category_name); ?></td>
-                 </tr>
-            <?php if($product->subcat_name):?>
-              <tr>
-                  <th>Sub-category</th>
-                  <td><?php echo html_escape($product->subcat_name); ?></td>
-              </tr>
-            <?php endif;?>
-                    <tr>
-                        <th>Product Name</th>
-                        <td><?php echo html_escape($product->product_name); ?></td>
-                    </tr>
-                    <tr>
-                        <th>Parts No.</th>
-                        <td><?php echo html_escape($product->parts);?></td>
-                    </tr>
-              <tr>
-                  <th>SKU</th>
-                  <td><?php echo html_escape($product->sku);?></td>
-              </tr>
-              <tr>
-                  <th>Unit Type</th>
-                  <td><?php echo html_escape($product->unit); ?></td>
-              </tr>
-                    <tr>
-                        <th>Brand</th>
-                        <td><?php echo html_escape($product->brand_name); ?></td>
-                    </tr>
-                  <tr>
-                      <th>Model</th>
-                      <td><?php echo html_escape($product->model_name); ?></td>
-                  </tr>
-              <tr>
-                  <th>Associated Tag</th>
-                  <td><?php echo html_escape($product->tag);?></td>
-              </tr>
-              
-                                     
-</table>
-</div>
-</div>
-</div>
- <?php } ?>
+                </div>
+                <div class="col-sm-12 col-md-8">
+                    <div class="col-sm-6 col-md-6 " >
+                        <table class="table" width="100%" id="myTable">
+                            <tr>
+                                <th>Category</th>
+                                <td><?php echo html_escape($product->category_name); ?></td>
+                            </tr>
+                            <?php if($product->subcat_name):?>
+                                <tr>
+                                    <th>Sub-category</th>
+                                    <td><?php echo html_escape($product->subcat_name); ?></td>
+                                </tr>
+                            <?php endif;?>
+                            <tr>
+                                <th>Product Name</th>
+                                <td><?php echo html_escape($product->product_name); ?></td>
+                            </tr>
+                            <tr>
+                                <th>Parts No.</th>
+                                <td><?php echo html_escape($product->parts);?></td>
+                            </tr>
+                            <tr>
+                                <th>SKU</th>
+                                <td><?php echo html_escape($product->sku);?></td>
+                            </tr>
+                            <tr>
+                                <th>Unit Type</th>
+                                <td><?php echo html_escape($product->unit); ?></td>
+                            </tr>
+                            <tr>
+                                <th>Brand</th>
+                                <td><?php echo html_escape($product->brand_name); ?></td>
+                            </tr>
+                            <tr>
+                                <th>Model</th>
+                                <td><?php echo html_escape($product->model_name); ?></td>
+                            </tr>
+                            <tr>
+                                <th>Associated Tag</th>
+                                <td><?php echo html_escape($product->tag);?></td>
+                            </tr>
+
+
+                        </table>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
     </section>
     <?php echo $links;?>
 </div>
